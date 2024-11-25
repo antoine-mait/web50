f03222c091452c9000f444a069a9db3aab77031e
antoine-mait <antoine.maitrias@gmail.com>
2024-11-25 09:24:27 +0100

    wiki exercice

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-
+import markdown
 from . import util
 
+def markdown_convert(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None : 
+        return None
+    else:
+        return markdowner.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    html_content = markdown_convert(title)
+    if html_content == None:
+        return render(request, "encyclopedia/error.html",{
+            "message": "This entry doesn't exist"
+        })
+    else:
+        return render(request, "encyclopedia/entry.html" ,{
+                      "title":title,
+                      "content": html_content,
+        })
