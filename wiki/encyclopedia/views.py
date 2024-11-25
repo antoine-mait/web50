52e2d3c9478366f09ebfa500baec1fdbe59098bc
antoine-mait <antoine.maitrias@gmail.com>
2024-11-25 16:14:26 +0100

    wiki exercice

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import markdown
 from . import util
+import random
 
 def markdown_convert(title):
     content = util.get_entry(title)
@@ -26,3 +27,70 @@ def entry(request, title):
                       "title":title,
                       "content": html_content,
         })
+    
+def search(request):
+    if request.method == "POST" :
+        entry_search = request.POST["q"]
+        html_content = markdown_convert(entry_search)
+        if html_content is not None:
+            return render(request, "encyclopedia/entry.html" ,{
+            "title":entry_search,
+            "content": html_content,
+            })
+        else:
+            allEntries = util.list_entries()
+            recommendation = []
+            for entry in allEntries:
+                if entry_search.lower() in entry.lower():
+                    recommendation.append(entry)
+            return render(request,  "encyclopedia/search.html",{
+                "recommendation" : recommendation,
+            } )
+        
+def new_page(request):
+    if request.method =="GET":
+        return render(request, "encyclopedia/new.html")
+    else:
+        title = request.POST["title"]
+        content = request.POST["content"]
+        titleExist = util.get_entry(title)
+        if titleExist is not None:
+            return render(request, "encyclopedia/error.html",{
+                          "message":"Entry page already exists"
+                          })
+        else:
+            util.save_entry(title,content) 
+            html_content = markdown_convert(title)
+            return render(request, "encyclopedia/entry.html", {
+                "title": title,
+                "content": html_content,
+            })   
+        
+def edit(request):
+    if request.method == "POST":
+        title = request.POST["entry_title"]
+        content = util.get_entry(title)
+        return render(request,"encyclopedia/edit.html",{
+            "title": title,
+            "content": content, 
+        })
+
+def save_edit(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        util.save_entry(title, content)
+        html_content = markdown_convert(title)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content,
+        })   
+    
+def rand(request):
+    allEntries = util.list_entries()
+    rand_entry = random.choice(allEntries)
+    html_content = markdown_convert(rand_entry)
+    return render(request,  "encyclopedia/entry.html", {
+            "title": rand_entry,
+            "content": html_content,
+        })   
