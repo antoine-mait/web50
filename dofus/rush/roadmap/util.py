21b06a08ca945e8d8837bedb312b10192f9ac016
antoine-mait <antoine.maitrias@gmail.com>
2024-11-30 15:53:59 +0100

    stuff part

--- dofus/rush/roadmap/util.py
+++ dofus/rush/roadmap/util.py
@@ -3,27 +3,15 @@ import re
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
+# road page
 
 def list_entries():
     """
     Returns a list of all names of step entries.
     """
-    _, filenames = default_storage.listdir("entries")
+    _, filenames = default_storage.listdir("entries/road")
     return list(sorted(re.sub(r"\.html$", "", filename)
                 for filename in filenames if filename.endswith(".html")))
-
-
-def save_entry(title, content):
-    """
-    Saves an step entry, given its title and Markdown
-    content. If an existing entry with the same title already exists,
-    it is replaced.
-    """
-    filename = f"entries/{title}.html"
-    if default_storage.exists(filename):
-        default_storage.delete(filename)
-    default_storage.save(filename, ContentFile(content))
-
 
 def get_entry(title):
     """
@@ -31,7 +19,29 @@ def get_entry(title):
     entry exists, the function returns None.
     """
     try:
-        f = default_storage.open(f"entries/{title}.html")
+        f = default_storage.open(f"entries/road/{title}.html")
         return f.read().decode("utf-8")
     except FileNotFoundError:
         return None
+
+# Stuffs pages 
+
+def list_stuffs():
+    """
+    Returns a list of all names of step entries.
+    """
+    _, filenames = default_storage.listdir("entries/stuffs")
+    return list(sorted(re.sub(r"\.html$", "", filename)
+                for filename in filenames if filename.endswith(".html")))
+
+def get_stuffs(title):
+    """
+    Retrieves an step entry by its title. If no such
+    entry exists, the function returns None.
+    """
+    try:
+        f = default_storage.open(f"entries/stuffs/{title}.html")
+        return f.read().decode("utf-8")
+    except FileNotFoundError:
+        return None
+
--- dofus/rush/roadmap/views.py
+++ dofus/rush/roadmap/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from bs4 import BeautifulSoup
+
 from . import util
 
+import os
 
 # Create your views here.
 def home(request):
     return render(request, "roadmap/home.html")
 
-def builds(request):
-    return render(request, "roadmap/builds.html")
+def spreadsheet(request):
+    return render(request, "roadmap/spreadsheet.html")
 
 def roadmap(request):
     entries = util.list_entries()
@@ -23,5 +27,22 @@ def roadmap(request):
         "entry_content": entry_content,  # Pass the dictionary of contents
     })
 
-def spreadsheet(request):
-    return render(request, "roadmap/spreadsheet.html")
+def builds(request):
+    entries = util.list_stuffs()
+    entry_content = {}
+
+    # Create a dictionary with entry titles as keys and their contents as values
+    for entry in entries:
+        title = entry
+        entry_content[title] = util.get_stuffs(title)
+
+    # Path to your images folder
+    image_folder = 'roadmap/static/images/stuffs'
+    image_files = os.listdir(image_folder)
+    image_files = [f for f in image_files if f.endswith(('.jpg', '.png', '.gif'))]  # Filter for image files
+
+    return render(request, "roadmap/stuffs.html", {
+        "entries": entries,
+        "entry_content": entry_content,  # Pass the dictionary of contents
+        'image_files': image_files,
+    })
